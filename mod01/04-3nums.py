d9f7777a5f3a22573b77dc8121ad4e8e9ee4623a
Niko Rahikainen <nikvadev@tuta.com>
2026-08-18 13:17:38 +0300

    04-Done

---
+++ mod01/04-3nums.py
@@ -0,0 +1,11 @@
+luku1 = int(input("Anna ensimmäinen numero (1/3): "))
+luku2 = int(input("Anna toinen numero (2/3): "))
+luku3 = int(input("Anna kolmas numero (3/3): "))
+
+summa = luku1 + luku2 + luku3
+tulo = luku1 * luku2 * luku3
+keskiarvo = (luku1 + luku2 + luku3) / 3
+
+print("Lukujen summa on: "+str(summa))
+print("Lukujen tulo on: "+str(tulo))
+print("Lukujen keskiarvo on: "+str(keskiarvo))
