40d027a642cea434fe91b851e44b15d0f177386e
Niko Rahikainen <nikvadev@tuta.com>
2026-08-21 11:18:37 +0300

    04_04-Done

--- mod04/04-leap-year-check.py
+++ mod04/04-leap-year-check.py
@@ -1,8 +1,16 @@
+# Ottaa ohjelmalle ilmoitetun vuosiluvun ja laskee onko kyseessä karkausvuosi.
+# Jos vuosi on jaollinen neljällä tai 100 sekä 400, kyseessä on karkausvuosi.
 vuosi = (input("Anna vuosiluku: "))
-vuosiNumero = int(vuosi) / 4
-print(str(vuosiNumero))
+lukuJaettuna4 = int(vuosi) / 4
+lukuJaettuna100 = int(vuosi) / 100
+lukuJaettuna400 = int(vuosi) / 400
 
-if vuosiNumero.is_integer():
-    print("Karkausvuosi")
+if lukuJaettuna100.is_integer():
+    if lukuJaettuna400.is_integer():
+        print("Ilmoittamasi vuosiluku on karkausvuosi.")
+    else:
+        print("Ilmoittamasi vuoliluku ei ole karkausvuosi.")
+elif lukuJaettuna4.is_integer():
+    print("Ilmoittamasi vuosiluku on karkausvuosi.")
 else:
-    print("Normaalivuosi")
+    print("Ilmoittamasi vuoliluku ei ole karkausvuosi.")
