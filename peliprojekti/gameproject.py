7abccaf33dd6f4bdf2ad5349d34bf708aa8fbb93
Niko Rahikainen <nikvadev@tuta.com>
2026-08-21 08:54:00 +0300

    Peliprojekti #1

---
+++ peliprojekti/gameproject.py
@@ -0,0 +1,7 @@
+playerName = input("Please insert your name: ")
+playerAge = input("Please insert your age: ")
+
+print("------------------")
+print("Welcome, " + playerName)
+print("Age: (" + playerAge + ")")
+print("------------------")
