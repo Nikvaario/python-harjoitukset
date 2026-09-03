4c32b1709dd94e3738c6b3cc543e8341776271d5
Nikva <nikvadev@tuta.com>
2026-09-03 23:53:49 +0300

    Peliprojekti–2-Done

--- peliprojekti/README.md
+++ peliprojekti/README.md
@@ -1,1 +1,16 @@
 # Ohjelmisto 1 - Peliprojekti
+
+**Niko Rahikainen**
+
+## Projekti 1
+Tein peliprojektin ensimmäisen osion 21.8. jolloin lisäsin ohjelmaan pelaajan nimen sekä iän kysymisen, jonka jälkeen ohjelma kutsuu pelaajan tervetulleeksi ja avaa pienen stats näkymän, jossa lukee pelaajan nimi ja ikä.
+
+## Projekti 2
+Tein peliprojektin toisen osion 3.9. jolloin lisäsin ohjelmaan ikätarkistuksen, joka pysäyttää ohjelman mikäli käyttäjä ilmoittaa iäkseen 12. Importtasin tätä varten sys moduulin, jonka kautta voi näppärästi käyttää sys.exit() komentoa lopettamaan ohjelman.
+
+Tein myös erilaisia komentoja käyttäjälle, jonka avulla hän saa lisätietoa pelaajasta sekä ympäristöstä:
+-| Inventory: Kertoo mitä tarvikkeita pelaajalla on.
+-| Check cash: Kertoo tarkemmin pelaajan käteistilanteen eri kolikkojen määrässä.
+-| Surrounding: Kertoo pelaajan nykyisen ympäristön.
+
+Komentoja varten lisäsin stats valikkoon yksinkertaisen cash osion sekä kuvauksen pelaajan inventoryn käytön määrästä.
--- peliprojekti/gameproject.py
+++ peliprojekti/gameproject.py
@@ -1,7 +1,36 @@
+import sys
+
+# Asks player's name and age
 playerName = input("Please insert your name: ")
 playerAge = input("Please insert your age: ")
+cash = 250
 
+# Checks if player is old enough to play the game
+playerAge_int = int(playerAge)
+if (playerAge_int <= 12):
+    print("You are underage to play this game. Ending program!")
+    sys.exit()
+
+# Prints a welcome text and player stats
 print("------------------")
 print("Welcome, " + playerName)
 print("Age: (" + playerAge + ")")
+print("Cash: (" + str(cash) + ")")
+print("Your inventory is half full")
 print("------------------")
+
+# Asks the player for different commands
+command = input("What would you like to do? (Inventory, Check cash, Surrounding or Cancel): ")
+print("------------------")
+while command != "cancel":
+    if command == "Inventory":
+        print("You have a small rope, 5 days of rations and a survivor kit in your backbag!")
+    elif command == "Check cash":
+        print("You have 40 silver coins and 10 gold coins, totaling 250 cash")
+    elif command == "Surrounding":
+        print("You are in a spruce forest near your campsite, currently sitting next to a campfire with food warming up.")
+    else:
+        print("Unknown command, try again!")
+    command = input("What would you like to do? (Inventory, Check cash, Surrounding or Cancel): ")
+    print("------------------")
+
